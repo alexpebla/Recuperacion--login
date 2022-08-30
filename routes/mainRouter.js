976bf7ac1e012dbf7441d07cd45a0d222c8eceb1
Alexander P <alexpebla@gmail.com>
2022-08-30 11:50:17 -0500

    ejs

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,11 +3,8 @@ const router = require('express').Router()
 
 const mainController = require('../controllers/mainController')
 
-
-
 router.get ("/login",mainController.login)
 
 router.post ("/login",mainController.loginProcess)
-
 
 module.exports = router;
